09a4536b517476e3cb80a7f5b47d4c206bcb8806
Sheltonjerrid <jerridshelton1991@gmail.com>
2026-01-31 19:54:09 -0500

    Add app.js
    Add functionality to navigate through screens with loading simulation.

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+let current = 0;
+const screens = document.querySelectorAll('.screen');
 
+function show(i){
+  screens.forEach(s => s.classList.remove('active'));
+  screens[i].classList.add('active');
+}
+
+function next(){
+  current++;
+
+  // fake loading screen
+  if(screens[current].classList.contains('loading')){
+    show(current);
+    setTimeout(()=>{ current++; show(current); }, 2500);
+  } else {
+    show(current);
+  }
+}
+
+function setMoveType(val){
+  next();
+}
+
+function setHomeSize(val){
+  next();
+}
+
+function setMoveDate(val){
+  next();
+}
+
+show(current);
